460f9c40b2f36394be9da0ec206a08eaf214b61c
Lukakev <128063091+Lukakev@users.noreply.github.com>
2023-07-12 19:13:24 +0000

    Add files via upload

---
+++ index.py
@@ -0,0 +1,6 @@
+sub="what i learned"
+print(sub+ " with davit tyeshelashvili")
+what_learnt="i have learnt humans use code to give instructions to machines"
+print(what_learnt" also the print instruction displays a message on the screen ")
+fun_fact="did you know a part of text can be called a string strings require quotacion marks "
+print(fun_fact+"and print as a statment is used to send a value to the screen")
